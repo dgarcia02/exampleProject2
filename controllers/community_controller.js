df5e40645cc9d9a72521484d5f54e73e0d81c742
Dulce Garcia <dulcegarcia@Dulces-Air.lan1>
2021-07-25 21:43:26 -0500

    created community schema

--- controllers/community_controller.js
+++ controllers/community_controller.js
@@ -27,7 +27,60 @@ community.get('/', (req, res) => {
 })
 
 // =========== SEED ROUTE ===========//
-
+community.get('/setup/seed', (req, res) => {
+    Community.deleteMany({}, () => {})
+    Community.create(
+        [
+            {
+                name: 'Talia Daniel',
+                dateOfBirth: {
+                    month: 'February',
+                    day: 12,
+                    year: 2002,
+                    },
+                gender: 'female',
+                pronouns: 'she/her',
+                location: {
+                    state: 'Ohio',
+                    zip: 45768
+                    },
+                points: 0,
+                img: "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
+                posts: {
+                    date: 'May 16, 2021',
+                    entry:'',
+                    feeling: 'Good',
+                    img: "../public/imgs/biking.jpg"
+                }
+            },
+            {
+                name: 'Josh Lawrence',
+                dateOfBirth: {
+                    month: 'September',
+                    day: 20,
+                    year: 1997,
+                    },
+                gender: 'Male',
+                pronouns: 'He/Him',
+                location: {
+                    state: 'New York',
+                    zip: 11226
+                    },
+                points: 0,
+                img: "../public/imgs/profile_m2.jpg",
+                posts: {
+                    date: 'March 23, 2021',
+                    entry: 'Just went out for a well needed run with amazing weather! Keep up the great work everyone!',
+                    feeling: 'Great',
+                    img: "../public/imgs/running_m2.jpg"
+                }
+            }
+        ],
+        (error, data) => {
+            res.redirect('/community')
+        }
+    )
+})
 
 
 module.exports = community;
--- models/community.js
+++ models/community.js
@@ -3,12 +3,31 @@ const Schema = mongoose.Schema
 
 const communitySchema = new Schema({
     name: { type: String, required: true },
-    dateOfBirth: String,
+    dateOfBirth: {
+        type: Object,
+        required: true,
+        month: String,
+        day: Number,
+        year: Number
+    },
     gender: String,
     pronouns: String,
-    location: { type: String, required: true },
+    location: {
+        type: Object,
+        required: true,
+        state: String,
+        zip: Number,
+    },
     points: Number,
-    img: String
+    img: String,
+    posts: {
+        type: Object,
+        date: String,
+        entry: String,
+        // great, good, tired, exhausted, can barely walk
+        feeling: String,
+        img: String
+    }
 })
 
 // this sets up the collection in the database
